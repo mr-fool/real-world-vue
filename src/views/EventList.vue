534b1fcfa2cf0ceb167c8bae4f82b12a34529598
mr-fool <mr-fool@users.noreply.github.com>
2020-04-15 22:44:00 -0600

    axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,15 +1,32 @@
 <template>
     <div>
         <h1>Events Listing </h1>
-        <EventCard/>
+        <EventCard v-for="event in events" :key="event.id" :event="event" />
         <BaseIcon/>
     </div>
 </template>
 <script>
-import EventCard from '@/components/EventCard.vue'
-export default {
-    components: {
+    import EventCard from '@/components/EventCard.vue'
+    import axios from 'axios' // <--- brings in the axios library
+    
+    export default {
+      components: {
         EventCard
-    },
-}
-</script>
+      },
+      data() {
+          return {
+              events: []
+          }
+      },
+      created() {
+        axios
+          .get('http://localhost:3000/events')  // Does a get request
+          .then(response => {
+            this.events = response.data
+          })
+          .catch(error => {
+            console.log('There was an error:', error.response) // Logs out the error
+          })
+      }
+    }
+    </script>
